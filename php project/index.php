759b4f81035c41ffaea52cab9db401551b71196c
Eng.Tala <talabts33@gmail.com>
2026-04-19 10:23:00 +0200

    Updat

--- php project/index.php
+++ php project/index.php
@@ -1,59 +1,143 @@
-<?php echo "Hello, World!";
-$var1="Hello";
-$var2="hello";
-if(strtolower($var1) == strtolower($var2)) {
-    echo'$var1 is Equal $var2';
-}else{
+<?php
+echo "Hello, World!";
+$var1 = "Hello";
+$var2 = "hello";
+if (strtolower($var1) == strtolower($var2)) {
+    echo '$var1 is Equal $var2';
+} else {
     echo 'Not Equal';
 }
-echo str_replace("world","Tala","Hello world");
-echo str_ireplace("world","Tala","Hello World"); 
-$x=4;
-if($x>3)echo"X is grater than 3";
+echo str_replace("world", "Tala", "Hello world");
+echo str_ireplace("world", "Tala", "Hello World");
+$x = 4;
+if ($x > 3)
+    echo "X is grater than 3";
+echo "<br>";
 ////////////////////////////////
-$h=20;
-if($h<"10"){
-    echo"Have a good Morning";
+$h = 20;
+if ($h < "10") {
+    echo "Have a good Morning";
+} else if ($h < "20") {
+    echo "Have a good day";
+} else {
+    echo "Have a good night";
 }
-else if($h<"20"){ 
-    echo"Have a good day";
-}
-else{
-    echo"Have a good night";
-}
+echo "<br>";
 //////////////////////////////////////
-if($h<"10"):
-    echo"Have a good Morning";
+if ($h < "10"):
+    echo "Have a good Morning";
 
-elseif($h<"20")://=> لازم بتكوم متصلة 
-    echo"Have a good day";
+elseif ($h < "20")://=> لازم بتكوم متصلة 
+    echo "Have a good day";
 
 else:
-    echo"Have a good night";
+    echo "Have a good night";
 endif;
+echo "<br>";
 //////////////////////////////////////////////////
 //Switch 
-$favColor ='red';
-switch($favColor) {
-    case"red":
-    echo"Your fav color red";
-    break;
-    case"blue":
-    echo"Your fav color blue";
-    break;
+$favColor = 'red';
+switch ($favColor) {
+    case "red":
+        echo "Your fav color red";
+        break;
+    case "blue":
+        echo "Your fav color blue";
+        break;
     default:
-    echo "";
+        echo "";
 }
+echo "<br>";
 ////////////////////////////////////////////////////
 //loops
-for($x=0;$x<=10;$x++){
-    echo"the number is : $x ";
+for ($x = 0; $x <= 10; $x++) {
+    echo "the number is : $x ";
+    echo "<br>";
 }
 
-for($x= 0;$x<= 10;$x++):
-    echo"the number is : $x ";
+for ($x = 0; $x <= 10; $x++):
+    echo "the number is : $x ";
+    echo "<br>";
 endfor;
+echo "<br>";
 /////////////////////////////////////////
+//function
+function writeMsg()
+{
+    echo "Hello, World!";
+}
+writeMsg(); // call the function
+//////////////////////////////////////
+//function argument 
+echo "<br>";
+function personName($fname, $year)
+{
+    echo " My name is $fname , Born in $year. <br>";
+}
+personName("tala", 2005);
+personName("salma", 2007);
+///////////////////////////////////////////
+//function default value parameters
+function personData($id, $name, $age = "unknown")
+{
+    echo "$id : $name:  $age <br>";
+}
+personData(1, "tala", 18);
+personData(2, "salma");
+/////////////////////////////////
+//  declare(strict_types=1);
+function addNumber(int $a, int $b)
+{
+    return $a + $b;
+}
+echo addNumber(1, 2);
+///////////////////////////////////////
+function setHeight(int $minHeight = 50)
+{
+    echo "The height is $minHeight <br>";
+
+}
+setHeight(350);
+setHeight();// => will use the data type
+//////////////////////////////////////////////
+//Function Return Type
+function sum(int $a, int $b)
+{
+    return $a + $b;
+}
+echo sum(1, 2);
+echo "<br>";
+//////////////////////////////////////////
+// return type declarations 
+function sumFloat(float $a, float $b): float
+{
+    return $a + $b;
+}
+echo sumFloat(1.5, 4.2);
+echo "<br>";
+function sumInt(float $a, float $b): int
+{
+    return $a + $b;
+}
+echo sumInt(2.5, 2.2);
+echo "<br>";
+//////////////////////////////////////////////
+//passing Argument by refrence 
+function addFive(&$value)
+{
+    $value += 5;
+
+}
+$num = 2;
+addFive($num);
+echo " <br> $num";
+///////////////////////////////////////////////////
+//Date and Time function 
+echo"<br>";
+echo "today ".date("Y-m-d")."<br>";
+echo "today ".date("Y.m.d")."<br>";
+echo "today ".date("Y/m/d")."<br>";
+echo "today ".date("Y");
 
 
 
